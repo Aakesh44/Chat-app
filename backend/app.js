02ea0edf886f4d8837391bfbe10dde938d2440c2
Aakesh44 <aakeshviswanathan@gmail.com>
2023-10-06 15:35:11 +0530

    p

--- backend/app.js
+++ backend/app.js
@@ -72,10 +72,10 @@ app.use(errorHandler.errorHandler)
 const PORT = process.env.PORT || 5000
 const server = app.listen(PORT,()=>{console.log(`your app is running on ${PORT}`)})
 
-const io = socketIo(server,{
-    pingTimeout: 60000,
+const io = require('socket.io')(server, {
+    // pingTimeout: 60000,
     cors: {
-        origin: '*'
+        origin: "*"
     }
 })
 
